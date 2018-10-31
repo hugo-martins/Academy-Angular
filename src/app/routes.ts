23efba85322bbddf580fb367435e52ffc875bbe3
mikaelgba <33075036+mikaelgba@users.noreply.github.com>
2018-10-30 21:50:30 -0300

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,12 +6,21 @@ import { ViewEquipamentoComponent } from './equipamento/view-equipamento/view-eq
 //import { EditEquipamentoComponent } from './equipamento/edit-equipamento/edit-equipamento.component';
 //import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { ListAcademiaComponent } from './academia/list-academia/list-academia.component';
+import { FormAcademiaComponent } from './academia/form-academia/form-academia.component';
+import { ViewAcademiaComponent } from './academia/view-academia/view-academia.component';
+
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   { path: 'equipamentos' , component: ListEquipamentoComponent},
   { path: 'equipamentos/new', component: FormEquipamentoComponent},
   { path: 'equipamentos/:id', component: ViewEquipamentoComponent},
   //{ path: 'equipamentos/edit/:id', component: EditEquipamentoComponent},
-  //{ path: 'dashboard', component: DashboardComponent}
+  //{ path: 'dashboard', component: DashboardComponent},
+  //{ path: 'academias/edit/:id', component: EditAcademiaComponent},
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: 'academias' , component: ListAcademiaComponent},
+  { path: 'academias/new', component: FormAcademiaComponent},
+  { path: 'academias/:id', component: ViewAcademiaComponent},
 
 ];
